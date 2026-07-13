3ff0160986b80f9764ea5727e3bc237f2e9293b7
Owen Lusis <owen00064@gmail.com>
2026-07-13 16:08:17 +1000

    hyprland: add computer specific rules

--- home/.config/hypr/hyprland.lua
+++ home/.config/hypr/hyprland.lua
@@ -5,3 +5,35 @@ require("modules.autostart")
 require("modules.general")
 require("modules.misc")
 
+local hostname = io.popen("hostname"):read("*l")
+local isLaptop = hostname == "nixos"
+
+if isLaptop then
+    hl.monitor({
+        output = "eDP-1",
+        mode = "preferred",
+        position = "auto",
+        scale = 1.67,
+    })
+else
+    hl.workspace_rule({ workspace = "1", monitor = "eDP-1", default = true})
+    hl.workspace_rule({ workspace = "2", monitor = "eDP-1"})
+    hl.workspace_rule({ workspace = "3", monitor = "eDP-1"})
+    hl.workspace_rule({ workspace = "4", monitor = "HDMI-A-1", default = true})
+    hl.workspace_rule({ workspace = "5", monitor = "HDMI-A-1"})
+    hl.workspace_rule({ workspace = "6", monitor = "HDMI-A-1"})
+
+    hl.monitor({
+        output = "eDP-1",
+        mode = "preferred",
+        position = "auto",
+        scale = 1.33,
+    })
+
+    hl.monitor({
+        output = "HDMI-A-1",
+        mode = "preferred",
+        position = "auto-center-left",
+        scale = 1,
+    })
+end
