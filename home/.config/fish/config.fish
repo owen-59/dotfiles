764825756228f55c8f9d578446cd2b71ccdec639
Owen Lusis <owen00064@gmail.com>
2026-07-20 17:06:06 +1000

    fish: modify tailscale exit nodes abbrs

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -5,8 +5,8 @@ set -g fish_key_bindings fish_vi_key_bindings
 if status is-interactive
     abbr --add n nvim
     abbr --add c cd
-    abbr --add exitup sudo tailscale set --exit-node=home-server
-    abbr --add exitdown sudo tailscale set --exit-node=
+    abbr --add exitup tailscale set --exit-node=server
+    abbr --add exitdown tailscale set --exit-node=
 
     zoxide init --cmd cd fish | source
     direnv hook fish | source
