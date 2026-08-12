5bd1d7aa34841c4e3b8192cd50f1fe5e09f5d0dc
Owen Lusis <owen00064@gmail.com>
2026-08-12 12:12:02 +1000

    nixos: remove kernel substituters

--- nixos/modules/laptop.nix
+++ nixos/modules/laptop.nix
@@ -34,16 +34,4 @@
     services.resolved.enable = true;
     networking.nameservers = [ "127.0.0.1" ];
     networking.networkmanager.dns = "systemd-resolved";
-
-    nix.settings = {
-        substituters = [
-            "https://nixos-apple-silicon.cachix.org"
-        ];
-        trusted-substituters = [
-            "https://nixos-apple-silicon.cachix.org"
-        ];
-        trusted-public-keys = [
-            "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
-        ];
-    };
 }
